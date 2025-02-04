0beace407850961e24660780affbf5b9d1a8a838
Erikas Minelga <erikas.minelga96@gmail.com>
2025-02-04 05:55:48 +0000

    Added background image for the hero section

--- src/gallery.js
+++ src/gallery.js
@@ -10,16 +10,19 @@ galleryControls.forEach(button => {
 
         counter = parseInt(button.dataset.index);
 
-        //Make the active control have solid fill
-        button.children[0].classList.remove("fa-regular");
-        button.children[0].classList.add("fa-solid");
-        
-        //And remove the solid background from the previously clicked button
-        galleryControls[previous].children[0].classList.remove("fa-solid");
-        galleryControls[previous].children[0].classList.add("fa-regular");
+        if(counter !== previous)
+        {
+            //Make the active control have solid fill
+            button.children[0].classList.remove("fa-regular");
+            button.children[0].classList.add("fa-solid");
+            
+            //And remove the solid background from the previously clicked button
+            galleryControls[previous].children[0].classList.remove("fa-solid");
+            galleryControls[previous].children[0].classList.add("fa-regular");
 
-        previous = counter;
+            previous = counter;
 
-        galleryItems.style.transform = `translateX(calc(-25% * ${counter}))`;
+            galleryItems.style.transform = `translateX(calc(-25% * ${counter}))`;
+        }
     });
 });
